b05f2ff8d34dd6e9139ee86d60342b9bccfd8cb4
Sammy Gonzalez <20673@my4county.net>
2021-08-25 09:59:15 -0400

    Added my ASCII Art Project and HelloWorld

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author sgonzalez
+ */
+public class HelloWorld {
+
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        System.out.println("    .-\"-.");
+        System.out.println("   /|6 6|\\");
+        System.out.println("  {/(_0_)\\}");
+        System.out.println("   _/ ^ \\_");
+        System.out.println("  (/ /^\\ \\)-'");
+        System.out.println("   \"\"' '\"\"");
+        System.out.println("\n   FIDO");
+       
+    }
+    
+}
